302fade08c64b6ef70fa018ab906b60cf745d03d
MonsieurGlock <chinnakitglock@gmail.com>
2020-09-10 14:39:47 +0700

    small and palindrome commit

---
+++ palindrome2.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+    int k=0;
+    char a[30];
+    char *p1,*p2;
+    p1 = a;
+    p2 = a;
+    scanf("%s",a);
+    while(*p2 != '\0'){
+        p2++;
+    }
+    p2 = p2-1;
+    while(p1 < p2){
+        //printf("%c\n",*p1);
+        //printf("%c\n",*p2);
+        if(*p1 != *p2){
+            printf("Not palindrom");
+            break;
+        }
+        if(*p1 == *p2){
+            k = 1;
+        }
+        p1++;
+        p2--;
+    }
+    if(k == 1){
+        printf("Palindrom");
+    }
+    return 0;
+}
---
+++ small.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    char a[30];
+    char *p;
+    scanf("%s",a);
+    p = a;
+    while(*p != '\0'){
+        if(*p >= 'A' && *p <='Z'){
+            *p = (*p - 'A')+'a';
+        }
+        else if(*p >= 'a' && *p <='z'){
+            *p = (*p - 'a')+'A';
+        }
+        printf("%c",*p);
+        p++;
+    }
+    return 0;
+}
